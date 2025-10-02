7e2d866987191e5ee12a7ffb5afdeb0e7c29f0bf
ベトグェン <beto.hanoi@gmail.com>
2025-10-02 08:45:06 +0000

    đặt tên luồng này là Y-L01-VT06

--- src/components/Y-L01-dialog.tsx
+++ src/components/Y-L01-dialog.tsx
@@ -920,7 +920,7 @@ export function YL01Dialog({
     <>
       <Dialog open={isOpen} onOpenChange={(open) => { onOpenChange(open); if (!open) setStep(1); }}>
           {children && <DialogTrigger asChild>{children}</DialogTrigger>}
-          <DialogContent className="sm:max-w-4xl" id="Y-L01-VT05">
+          <DialogContent className="sm:max-w-4xl" id="Y-L01-VT06">
               {renderDialogContent()}
           </DialogContent>
       </Dialog>
