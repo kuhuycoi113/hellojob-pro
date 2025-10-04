a897c441d6cc006cefbe910d5b1f2561542634eb
ベトグェン <beto.hanoi@gmail.com>
2025-10-04 01:13:22 +0000

    bắt đầu

--- src/components/layout/layout-manager.tsx
+++ src/components/layout/layout-manager.tsx
@@ -36,12 +36,14 @@ export function LayoutManager({ children }: { children: React.ReactNode }) {
     const isCallPage = pathname.startsWith('/goi-video') || pathname.startsWith('/goi-thoai');
     const isPartnerPage = pathname.startsWith('/doi-tac') || pathname.startsWith('/partner');
     const isNtdLandingNdPage = pathname === '/nha-tuyen-dung/landing/nd';
+    const isTuyenDungTokuteiPage = pathname === '/nha-tuyen-dung/landing/tuyen-dung-tokutei';
 
-    const excludedCtaPages = ['/', '/gioi-thieu', '/nha-tuyen-dung', '/nhuong-quyen', '/viec-lam', '/nha-tuyen-dung/dang-ky', '/nha-tuyen-dung/landing/nd', '/nha-tuyen-dung/landing/can-bo-tuyen-dung'];
+
+    const excludedCtaPages = ['/', '/gioi-thieu', '/nha-tuyen-dung', '/nhuong-quyen', '/viec-lam', '/nha-tuyen-dung/dang-ky', '/nha-tuyen-dung/landing/nd', '/nha-tuyen-dung/landing/can-bo-tuyen-dung', '/nha-tuyen-dung/landing/tuyen-dung-tokutei'];
     
     // Determine whether to show CTAs based on client-side path
     const showDefaultCtas = isClient && !isCallPage && !isPartnerPage && !excludedCtaPages.includes(pathname);
-    const showNtdLandingCta = isClient && isNtdLandingNdPage;
+    const showNtdLandingCta = isClient && (isNtdLandingNdPage || isTuyenDungTokuteiPage);
 
 
     useEffect(() => {
